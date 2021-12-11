eccfa1c8337ba7ccbddb844d31a9da43a9973e5f
Christian Mena <christian.91.gm@hotmail.com>
2021-12-11 18:10:13 -0500

    Delete environments.ts

--- frontend/nft-visualiser/src/environments/environments.ts
+++
@@ -1,7 +0,0 @@
-export const environments: {
-    MUMBAI_RPC: string;
-    CONTRACT_ADDRESS: string;
-} = {
-    MUMBAI_RPC: 'https://rpc-mumbai.maticvigil.com/',
-    CONTRACT_ADDRESS: '0xDC836Cbc358AeB0eE5f5944723A2A8418c9B9337',
-}
